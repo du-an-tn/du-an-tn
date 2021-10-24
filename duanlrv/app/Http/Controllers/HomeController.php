2524322c9eecafaddc1f46573ea48aaa125e47d2
wedontalk <86839817+wedontalk@users.noreply.github.com>
2021-10-24 16:20:56 +0700

    Update HomeController.php

--- duanlrv/app/Http/Controllers/HomeController.php
+++ duanlrv/app/Http/Controllers/HomeController.php
@@ -28,11 +28,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $category= DB::table('categories')->where('hidden','1')->where('id_nav','1')->orderby('id','desc')->get();
-        $category_meo= DB::table('categories')->where('hidden','1')->where('id_nav','6')->orderby('id','desc')->get();
-        $category_ca= DB::table('categories')->where('hidden','1')->where('id_nav','3')->orderby('id','desc')->get();
-        $category_chim= DB::table('categories')->where('hidden','1')->where('id_nav','4')->orderby('id','desc')->get();
-        $category_khac= DB::table('categories')->where('hidden','1')->where('id_nav','5')->orderby('id','desc')->get();
+        $category= DB::table('categories')->where('hidden','1')->where('id_nav','1')->orderby('id','ASC')->get();
+        $category_meo= DB::table('categories')->where('hidden','1')->where('id_nav','6')->orderby('id','ASC')->get();
+        $category_ca= DB::table('categories')->where('hidden','1')->where('id_nav','3')->orderby('id','ASC')->get();
+        $category_chim= DB::table('categories')->where('hidden','1')->where('id_nav','4')->orderby('id','ASC')->get();
+        $category_khac= DB::table('categories')->where('hidden','1')->where('id_nav','5')->orderby('id','ASC')->get();
         return view('pages.home')->with(compact('category','category_meo','category_ca','category_chim','category_khac'));
     }
 }
